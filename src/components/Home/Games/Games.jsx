1786f188ffd22de0f3ff4a5412eaecb0796b316b
Osariemen Daniel Osazee <joelifetang31@gmail.com>
2022-12-28 17:54:48 +0100

    switched to css and added gray out for game section

--- src/components/Home/Games/Games.jsx
+++ src/components/Home/Games/Games.jsx
@@ -1,53 +1,53 @@
 import React from 'react'
-import styles from './Games.module.css'
+import './Games.css'
 import cod1 from '../../../assets/Home/Game/cod1.jpg'
 
 const Games = () => {
     return (
-        <div className={styles.game__container}>
-            <div className={styles.game__container__title}>
+        <div className="game__container">
+            <div className="game__container__title">
                 <h1>Games</h1>
                 <ul>
-                    <li className={styles.game__container__title__active}>Popular</li>
+                    <li className="game__container__title__active">Popular</li>
                     <li>Recent</li>
                 </ul>
             </div>
 
-            <div className={styles.game__container__card__con}>
+            <div className="game__container__card__con">
 
-                <div className={styles.game__container__card__con__card}>
+                <div className="game__container__card__con__card">
                     <img src={cod1} alt="" />
                     <div>
                         <p>Call of Duty mobile</p>
-                        <p className={styles.game__container__card__con__card__btn}>Enter</p>
+                        <p className="game__container__card__con__card__btn">Enter</p>
                     </div>
                 </div>
-                <div className={styles.game__container__card__con__card}>
+                <div className="game__container__card__con__card game__container__card__con__gray">
                     <img src={cod1} alt="" />
                     <div>
                         <p>Call of Duty mobile</p>
-                        <p className={styles.game__container__card__con__card__btn}>Enter</p>
+                        <p className="game__container__card__con__card__btn">Enter</p>
                     </div>
                 </div>
-                <div className={styles.game__container__card__con__card}>
+                <div className="game__container__card__con__card game__container__card__con__gray">
                     <img src={cod1} alt="" />
                     <div>
                         <p>Call of Duty mobile</p>
-                        <p className={styles.game__container__card__con__card__btn}>Enter</p>
+                        <p className="game__container__card__con__card__btn">Enter</p>
                     </div>
                 </div>
-                <div className={styles.game__container__card__con__card}>
+                <div className="game__container__card__con__card game__container__card__con__gray">
                     <img src={cod1} alt="" />
                     <div>
                         <p>Call of Duty mobile</p>
-                        <p className={styles.game__container__card__con__card__btn}>Enter</p>
+                        <p className="game__container__card__con__card__btn">Enter</p>
                     </div>
                 </div>
-                <div className={styles.game__container__card__con__card}>
+                <div className="game__container__card__con__card game__container__card__con__gray">
                     <img src={cod1} alt="" />
                     <div>
                         <p>Call of Duty mobile</p>
-                        <p className={styles.game__container__card__con__card__btn}>Enter</p>
+                        <p className="game__container__card__con__card__btn">Enter</p>
                     </div>
                 </div>
 
--- src/components/Home/Navbar/Navbar.jsx
+++ src/components/Home/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import styles from './Navbar.module.css'
+import './Navbar.css'
 import { FiMenu } from 'react-icons/fi'
 import { motion } from "framer-motion"
 
@@ -20,39 +20,39 @@ const Navbar = () => {
     }
 
     return (
-        <div className={styles.navbar__container}>
-            <div className={styles.navbar__container__left}>
+        <div className="navbar__container">
+            <div className="navbar__container__left">
 
                 <h1>Matrix Games</h1>
 
             </div>
 
-            <div className={styles.navbar__container__mobile} onClick={handleToggle}>
-                <FiMenu className={styles.navbar__container__mobile__icon} />
+            <div className="navbar__container__mobile" onClick={handleToggle}>
+                <FiMenu className="navbar__container__mobile__icon" />
             </div>
 
             {toggle && (
                 <motion.div
                     variants={variants}
-                    className={styles.navbar__container__mobile__link}>
+                    className="navbar__container__mobile__link">
                     <li>Home</li>
                     <li>Games</li>
                     <li>Plans</li>
                     <li>Contact</li>
-                    <button className={styles.navbar__container__mobile__link__button__login}>Sign In</button>
-                    <button className={styles.navbar__container__mobile__link__button__signup}>Sign Up</button>
+                    <button className="navbar__container__mobile__link__button__login">Sign In</button>
+                    <button className="navbar__container__mobile__link__button__signup">Sign Up</button>
                 </motion.div>
             )}
 
-            <div className={styles.navbar__container__right}>
-                <ul className={styles.navbar__container__right__link}>
+            <div className="navbar__container__right">
+                <ul className="navbar__container__right__link">
                     <li>Home</li>
                     <li>Games</li>
                     <li>Plans</li>
                     <li>Contact</li>
                 </ul>
-                <div className={styles.navbar__container__right__button}>
-                    <button className={styles.navbar__container__right__button__login}>Sign In</button>
+                <div className="navbar__container__right__button">
+                    <button className="navbar__container__right__button__login">Sign In</button>
                     <button>Sign Up</button>
                 </div>
             </div>
