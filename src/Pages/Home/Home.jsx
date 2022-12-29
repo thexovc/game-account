8c224a2bee9e67dccd826cd99ca300f5b4d5bda7
Osariemen Daniel Osazee <joelifetang31@gmail.com>
2022-12-29 02:05:17 +0100

    completed howtouse section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Games from '../../components/Home/Games/Games'
+import HowToUse from '../../components/Home/HowToUse/HowToUse'
+import './Home.css'
 
 const Home = () => {
     return (
-        <>
+        <div className='home__container'>
             <Games />
-        </>
+            <HowToUse />
+        </div>
     )
 }
 
