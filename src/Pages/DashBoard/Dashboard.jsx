9154ad4fbfff6d17d3dc9fa4f97f8e7c9ac03884
Osariemen Daniel Osazee <68135656+thexovc@users.noreply.github.com>
2023-01-09 11:17:58 +0100

    updated the home and dashboard pages

---
+++ src/Pages/DashBoard/Dashboard.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import MainDash from "../../components/Dashboard/MainDash/MainDash";
+import Navbar from "../../components/Dashboard/Navbar/Navbar";
+import RightSide from "../../components/Dashboard/RigtSide/RightSide";
+import Sidebar from "../../components/Dashboard/Sidebar";
+
+import "./Dashboard.css";
+
+const Dashboard = () => {
+  const [expanded, setExpanded] = useState(!false);
+
+  const expandState = (state) => {
+    setExpanded(state);
+  };
+  return (
+    <div className="App">
+      <div className="AppHead">
+        <Navbar expandState={expandState} />
+      </div>
+
+      <div className="AppGlass">
+        <Sidebar expandState={expanded} />
+        <MainDash />
+        <RightSide />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,39 +1,30 @@
-import React, { useState } from 'react'
-import Games from '../../components/Home/Games/Games'
-import HowToUse from '../../components/Home/HowToUse/HowToUse'
-import Video from '../../components/Home/Video/Video'
-import Heroarea from '../../components/Home/Heroarea/Heroarea'
-import Stats from '../../components/Home/Stats/Stats'
-import Wcu from '../../components/Home/Wcu/Wcu'
-import Plans from '../../components/Home/Plans/Plans'
-import Cta from '../../components/Home/Cta/Cta'
-import Trx from '../../components/Home/Trx/Trx'
-import './Home.css'
-import Footer from '../../components/Home/Footer/Footer'
-import Navbar from '../../components/Home/Navbar/Navbar'
+import React from "react";
+import Games from "../../components/Home/Games/Games";
+import HowToUse from "../../components/Home/HowToUse/HowToUse";
+import Video from "../../components/Home/Video/Video";
+import Heroarea from "../../components/Home/Heroarea/Heroarea";
+import Stats from "../../components/Home/Stats/Stats";
+import Wcu from "../../components/Home/Wcu/Wcu";
+import Plans from "../../components/Home/Plans/Plans";
+import Cta from "../../components/Home/Cta/Cta";
+import Trx from "../../components/Home/Trx/Trx";
+import "./Home.css";
+import Hero from "../../components/Home/Heroarea/Hero";
 
-const Home = () => {
-    const [toggle, setToggle] = useState(false)
+const Home = ({ expandState }) => {
+  return (
+    <div className="home__container">
+      <Hero expandState={expandState} />
+      <Games />
+      <Stats />
+      <Cta />
+      <Wcu />
+      <Plans />
+      <HowToUse />
+      <Video />
+      <Trx />
+    </div>
+  );
+};
 
-
-
-    return (
-        <>
-            <Navbar toggle={toggle} setToggle={setToggle} />
-            <div className='home__container' onClick={() => setToggle(!toggle)}>
-                <Heroarea />
-                <Games />
-                <Stats />
-                <Cta />
-                <Wcu />
-                <Plans />
-                <HowToUse />
-                <Video />
-                <Trx />
-            </div>
-            <Footer />
-        </>
-    )
-}
-
-export default Home
+export default Home;
