45350a7eb401dea565e0f6a401be33d7951a0399
Osariemen Daniel Osazee <68135656+thexovc@users.noreply.github.com>
2023-01-11 13:31:57 +0100

    adding more changes to the profile page

--- src/Pages/DashBoard/Profile/Profile.jsx
+++ src/Pages/DashBoard/Profile/Profile.jsx
@@ -2,7 +2,10 @@ import React, { useState } from 'react'
 import Navbar from "../../../components/Dashboard/Navbar/Navbar";
 import Sidebar from '../../../components/Dashboard/Sidebar';
 import './Profile.css'
-import { AiFillStar } from 'react-icons/ai'
+import { AiFillShopping, AiFillStar } from 'react-icons/ai'
+import { BsCashCoin } from 'react-icons/bs'
+import { GrGamepad } from 'react-icons/gr'
+import { FaUserFriends } from 'react-icons/fa'
 import cod1 from '../../../assets/Home/Game/cod1.jpg'
 
 
@@ -27,18 +30,26 @@ const Profile = () => {
                             <div className='profile__title__text'>
                                 <h1>James Parker</h1>
                                 <p>Joined 2 months ago</p>
+
+                                <div className="profile__stats__star flex gap-1 p-2">
+                                    <AiFillStar className='text-[#FFCC4D]' />
+                                    <AiFillStar className='text-[#FFCC4D]' />
+                                    <AiFillStar className='text-[#FFCC4D]' />
+                                    <AiFillStar />
+                                </div>
                             </div>
                         </div>
 
                         <div className="profile__bio">
                             <p>
-                                ameconsectetur adipiscing elit. ac aliquet odiomattis.Classaptent taciti sociosqu ad litoratorquent per conubia nostra, Lorem ipsum
-                                ameconsectetur adipiscing elit. ac aliquet odiomattis.Classaptent taciti sociosqu ad litoratorquent per conubia nostra, Lorem ipsum
-                                ameconsectetur adipiscing elit. ac aliquet odiomattis.Classaptent taciti sociosqu ad litoratorquent per conubia nostra, Lorem ipsum
+                                I'm the hero of a thousand stories. I'm a superhero, an assassin a soldier.
+                                I've slain dragons and traveled through portals.
+                                I am a spartan, a commander. A king.
+                                I've saved a thousand worlds and countless more lives. What am I? I'm a gamer.
                             </p>
                         </div>
 
-                        <div className="profile__stats pb-5">
+                        {/* <div className="profile__stats pb-5">
                             <div>
                                 <p className='profile__stats__title'>How much made</p>
                                 <p className='profile__stats__text'>$1000</p>
@@ -52,13 +63,85 @@ const Profile = () => {
                                     <AiFillStar />
                                 </div>
                             </div>
+                        </div> */}
+
+                        <div className="profile__stats__card__con">
+                            <div className="profile__stats__card">
+                                <div className="profile__stats__card__text">
+                                    <p>Revenue</p>
+                                    <h1>$ 1284</h1>
+                                    <span> +5% <p> vs last 28 days</p></span>
+                                </div>
+                                <div className="profile__stats__card__icon">
+                                    <BsCashCoin />
+                                </div>
+                            </div>
+                            <div className="profile__stats__card">
+                                <div className="profile__stats__card__text">
+                                    <p>Games Sold</p>
+                                    <h1>284</h1>
+                                    <span> +20% <p> vs last 28 days</p></span>
+                                </div>
+                                <div className="profile__stats__card__icon">
+                                    <GrGamepad />
+                                </div>
+                            </div>
+                            <div className="profile__stats__card">
+                                <div className="profile__stats__card__text">
+                                    <p>Followers</p>
+                                    <h1>157</h1>
+                                    <span> +44% <p> vs last 28 days</p></span>
+                                </div>
+                                <div className="profile__stats__card__icon">
+                                    <FaUserFriends />
+                                </div>
+                            </div>
+                            <div className="profile__stats__card">
+                                <div className="profile__stats__card__text">
+                                    <p>Avaliable Games</p>
+                                    <h1>24</h1>
+                                    {/* <span> +5% <p> vs last 28 days</p></span> */}
+                                </div>
+                                <div className="profile__stats__card__icon">
+                                    <AiFillShopping />
+                                </div>
+                            </div>
+
+
                         </div>
 
                     </div>
 
                     <div className="profile__game pb-10">
                         <h1>Avaliable Games</h1>
+
+                        <div className="profile__game__category">
+                            <p className='bg-[#2A683E]'>Codm</p>
+                            <p>Pubg</p>
+                            <p>Fornite</p>
+                            <p>Apex Legends</p>
+                            <p>Clash Royale</p>
+                            <p>Warzone</p>
+                        </div>
+
+                        <h1>Codm</h1>
+
                         <div className="profile__game__card">
+
+                            <div className="seller__container__card__con__card">
+                                <div className='seller__container__card__con__card__desc cursor-pointer'>
+                                    <img src={cod1} alt="" />
+                                    <div className="seller__container__card__con__card__desc__text">
+                                        <p>Sold 234 account </p>
+                                        <p>Made $858</p>
+                                    </div>
+                                </div>
+                                <div className='seller__container__card__con__card__stats'>
+                                    <p className='cursor-pointer'>Ashalav</p>
+                                    <p className="flex gap-1 items-center border-2 p-1 rounded-md border-sky-600"><AiFillStar className='seller__star' /> 5.7</p>
+                                </div>
+                            </div>
+
                             <div className="seller__container__card__con__card">
 
                                 <div className='seller__container__card__con__card__desc cursor-pointer'>
@@ -87,6 +170,21 @@ const Profile = () => {
                                     <p className="flex gap-1 items-center border-2 p-1 rounded-md border-sky-600"><AiFillStar className='seller__star' /> 5.7</p>
                                 </div>
                             </div>
+                            <div className="seller__container__card__con__card">
+
+                                <div className='seller__container__card__con__card__desc cursor-pointer'>
+                                    <img src={cod1} alt="" />
+                                    <div className="seller__container__card__con__card__desc__text">
+                                        <p>Sold 234 account </p>
+                                        <p>Made $858</p>
+                                    </div>
+                                </div>
+                                <div className='seller__container__card__con__card__stats'>
+                                    <p className='cursor-pointer'>Ashalav</p>
+                                    <p className="flex gap-1 items-center border-2 p-1 rounded-md border-sky-600"><AiFillStar className='seller__star' /> 5.7</p>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
